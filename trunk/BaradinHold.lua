5cd13ccc7263c9548ed11c999d21186a1902a159
stassart <stassart@b14fde72-8f22-4ff2-a370-c9b2aa1f7878>
2010-12-22 13:15:53 +0000

    Added more trash debuffs.

--- trunk/BaradinHold.lua
+++ trunk/BaradinHold.lua
@@ -2,6 +2,10 @@ local zone = "Baradin Hold"
 
 --en_zone, debuffID, order, icon_priority, color_priority, timer, stackable, color, default_disable, noicon
 
+--Trash
+-- Demon Containment Unit
+GridStatusRaidDebuff:Debuff(zone, 89354, 1, 6, 5) --Arcane Amplifier
+
 --Argaloth
 GridStatusRaidDebuff:BossName(zone, 10, "Argaloth")
 GridStatusRaidDebuff:Debuff(zone, 88942, 11, 5, 5) --Meteor Slash
